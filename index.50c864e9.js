5a06fa1edf392e27c11942b6fbd1742e2109952c
Vladyslav Starodub <starodubvlad08@gmail.com>
2024-04-24 15:17:01 +0300

    make build

---
+++ index.50c864e9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,r=e.clientHeight;t.style.left=l/2+"px",t.style.top=r/2+"px",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.50c864e9.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+spider.style.left = wallWidth / 2 + "px";
+spider.style.top = wallHeight / 2 + "px";
+spider.style.transform = "translate(-50%, -50%)";
+
+//# sourceMappingURL=index.f75de5e1.js.map
